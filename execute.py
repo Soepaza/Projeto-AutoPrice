0992f4e182283f864300cbc094d89a609ed4cf0f
soepaza <soefreestyle@gmail.com>
2024-03-05 00:20:10 -0300

    Att

--- execute.py
+++ execute.py
@@ -1,16 +1,28 @@
 import pandas as pd
 import requests
+from bs4 import BeautifulSoup
 
-df = pd.read_excel("buscas.xlsx")
-# print(df)
+produtos = pd.read_excel('buscas.xlsx')
+lista_nomes = produtos['Nome'].to_list()
 
-li = df["Nome"].to_list()
-print(li)
+all_data = []  # Lista para armazenar os dados de todos os produtos
 
-for item in li:
-    url = requests.get(
-        f'https://www.buscape.com.br/search?q={item.replace(" ", "%")}')
-    print(url.content)
-    break
+for nome in lista_nomes:
+    url = f'https://www.buscape.com.br/search?q={nome}'
+    
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
 
-print(item[1])
+    results_store = soup.find_all('h3')
+    storeNames = [resultstore.string for resultstore in results_store]
+
+    results_prices = soup.find_all('p', class_='Text_Text__ARJdp Text_MobileHeadingS__HEz7L')
+    storePrice = [resultprice.string for resultprice in results_prices]
+
+    # Adicionar dados a all_data para cada loja e preço
+    for store, price in zip(storeNames, storePrice):
+        all_data.append({'Produto': nome, 'Loja': store, 'Preço': price})
+
+# Criar DataFrame final
+tabela_precos = pd.DataFrame(all_data)
+print(tabela_precos)
